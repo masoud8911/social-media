a71670b3bc9d23c33bad494274148755e037059e
masoud89111 <masoud.shakarami89@gmail.com>
2023-02-14 22:13:39 +0330

    create a social media

---
+++ account/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views import View
+
+
+class UserRegisterView(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
