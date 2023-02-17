d129a862d9894941661ccf7f3c823d593fa7483c
masoud8911 <masoud.shakarami89@gmail.com>
2023-02-17 17:34:57 +0330

    'Write email validation'

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,33 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class UserRegistrationForm(forms.Form):
     username = forms.CharField()
     email = forms.EmailField()
     password = forms.CharField()
+    password2 = forms.CharField(label='confirm password')
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = User.objects.filter(email=email).exists()
+        if user:
+            raise ValidationError('This email already exist')
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        user = User.objects.filter(username=username).exists()
+        if user:
+            raise ValidationError('This user name already exist')
+        return username
+
+    def clean(self):
+        cd = super().clean()
+        p1 = cd.get('password')
+        p2 = cd.get('password2')
+
+        if p1 and p2 and p1 != p2:
+            raise ValidationError('password must be match')
+        
