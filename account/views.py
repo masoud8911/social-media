349b048582987726a36d79cd6bd0a72d93b866d5
masoud8911 <masoud.shakarami89@gmail.com>
2023-02-22 19:21:30 +0330

    login logout user

--- account/views.py
+++ account/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class UserRegisterView(View):
     form_class = UserRegistrationForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home:index')
+        return super().dispatch(request, *args, **kwargs)
 
     def get(self, request):
         form = self.form_class()
@@ -21,3 +27,35 @@ class UserRegisterView(View):
             messages.success(request, 'Your registration successfully', 'success')
             return redirect('home:index')
         return render(request, 'account/register.html', {'form': form})
+
+
+class UserLoginView(View):
+    form_class = UserLoginForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home:index')
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, 'account/login.html', {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'User login is successfully', 'success')
+                return redirect('home:index')
+            messages.error(request, 'Username or password is wrong', 'warning')
+        return render(request, 'account/login.html', {'form': form})
+
+
+class UserLogoutView(LoginRequiredMixin, View):
+    def get(self, request):
+        logout(request)
+        messages.success(request, 'You are logout', 'success')
+        return redirect('home:index')
