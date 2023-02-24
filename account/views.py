89a09e09d32a5dcffdf2d8104b373e35a4d49511
masoud8911 <masoud.shakarami89@gmail.com>
2023-02-24 18:24:09 +0330

    add user profile and authenticate

--- account/views.py
+++ account/views.py
@@ -59,3 +59,10 @@ class UserLogoutView(LoginRequiredMixin, View):
         logout(request)
         messages.success(request, 'You are logout', 'success')
         return redirect('home:index')
+
+
+class UserProfileView(LoginRequiredMixin, View):
+    def get(self, request, user_id):
+        user = User.objects.get(pk=user_id)
+        posts = user.posts.all()
+        return render(request, 'account/profile.html', {'user': user, 'posts': posts})
