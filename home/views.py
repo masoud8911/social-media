f8310ee8be921dc7edac4655e5c7f246be9d4731
masoud8911 <masoud.shakarami89@gmail.com>
2023-02-24 15:29:25 +0330

    backend authentication for email

--- home/views.py
+++ home/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Post
+from django.contrib.auth.models import User
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'home/index.html')
+        posts = Post.objects.all()
+        return render(request, 'home/index.html', {'posts': posts})
+
+
+class PostDetailView(View):
+    def get(self, request, user_id, post_slug):
+        user = User.objects.get(pk=user_id)
+        post = Post.objects.get(user=user)
+        return render(request, 'home/detail.html', {'post': post})
