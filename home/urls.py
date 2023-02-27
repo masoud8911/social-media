95de2abd10e7e2acafa1b94afe1bf1f7b7f2cc78
masoud8911 <masoud.shakarami89@gmail.com>
2023-02-27 15:34:39 +0330

    create uodate post

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,9 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='index'),
-    path('detail/<int:user_id>/<slug:post_slug>', views.PostDetailView.as_view(), name='post_detail'),
-    path('delete/<int:post_id>', views.PostDeleteView.as_view(), name='post_delete'),
+    path('detail/<int:post_id>/<slug:post_slug>', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/delete/<int:post_id>/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('post/update/<int:post_id>/', views.PostUpdateView.as_view(), name='post_update'),
+    path('post/create/', views.PostCreateView.as_view(), name='post_create'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -4,6 +4,8 @@ from .models import Post
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
+from .forms import PostCreateUpdateForm
+from django.utils.text import slugify
 
 
 class HomeView(View):
@@ -13,9 +15,8 @@ class HomeView(View):
 
 
 class PostDetailView(View):
-    def get(self, request, user_id, post_slug):
-        user = get_object_or_404(User, pk=user_id)
-        post = get_object_or_404(Post, user=user)
+    def get(self, request, post_id, post_slug):
+        post = get_object_or_404(Post, pk=post_id, slug=post_slug)
         return render(request, 'home/detail.html', {'post': post})
 
 
@@ -29,3 +30,57 @@ class PostDeleteView(LoginRequiredMixin, View):
         else:
             messages.error(request, 'you cant delete this post', 'warning')
         return redirect('home:index')
+
+
+class PostUpdateView(LoginRequiredMixin, View):
+    form_class = PostCreateUpdateForm
+
+    def setup(self, request, *args, **kwargs):
+        self.post_current = get_object_or_404(Post, pk=kwargs['post_id'])
+        return super().setup(request, *args, **kwargs)
+
+    def dispatch(self, request, *args, **kwargs):
+        post = self.post_current
+        if not request.user.id == post.user.id:
+            messages.error(request, 'You cant update post', 'warning')
+            return redirect('home:index')
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        post = self.post_current
+        form = self.form_class(instance=post)
+        return render(request, 'home/update.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        post = self.post_current
+        form = self.form_class(request.POST, instance=post)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.slug = slugify(form.cleaned_data['body'][:33])
+            new_post.save()
+            messages.success(request, 'The post Updated successfully', 'success')
+            return redirect('home:post_detail', post.id, post.slug)
+        else:
+            messages.error(request, 'Input right information', 'warning')
+        return render(request, 'home/update.html', {'form': form})
+
+
+class PostCreateView(LoginRequiredMixin, View):
+    form_class = PostCreateUpdateForm
+
+    def get(self, request):
+        form = self.form_class
+        return render(request, 'home/create.html', {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.slug = slugify(form.cleaned_data['body'][:33])
+            new_post.user = request.user
+            new_post.save()
+            messages.success(request, 'Your new post created successfully', 'success')
+            return redirect('home:post_detail', new_post.id, new_post.slug)
+        else:
+            messages.error(request, 'Input right Info', 'warning')
+        return render(request, 'home/create.html', {'form': form})
