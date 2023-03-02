f74f6e6d46ee96a8f6288a41cb8828a03e199efb
masoud8911 <masoud.shakarami89@gmail.com>
2023-03-02 21:04:14 +0330

    add comment

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Post
-
+from .models import Post, Comment
 
 admin.site.register(Post)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'created', 'is_reply')
+    raw_id_fields = ('user', 'post', 'reply')
--- home/urls.py
+++ home/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('post/delete/<int:post_id>/', views.PostDeleteView.as_view(), name='post_delete'),
     path('post/update/<int:post_id>/', views.PostUpdateView.as_view(), name='post_update'),
     path('post/create/', views.PostCreateView.as_view(), name='post_create'),
-
 ]
