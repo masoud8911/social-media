11c7ad31fcc8986328239dfe273f605727ed2cc3
masoud8911 <masoud.shakarami89@gmail.com>
2023-03-08 10:09:00 +0330

    search in post

--- home/forms.py
+++ home/forms.py
@@ -21,3 +21,7 @@ class CommentReplyForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+
+class PostSearchForm(forms.Form):
+    search = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
